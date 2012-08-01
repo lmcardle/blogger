4bae62b27e405cd64898872cb5883c89d8de8697
Liam McArdle <liam@catholicsteward.com>
2012-08-01 10:15:15 -0700

    add comments to articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,8 @@
 class Article < ActiveRecord::Base
   attr_accessible :title, :body
   
+  has_many :comments
+  
   validates :title, presence: true
   validates :body, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Blogger::Application.routes.draw do
+  get "comments/create"
+
   resources :articles
+  resources :comments
 end
